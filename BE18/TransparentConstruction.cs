c637b0e3ffdce7c7e3124754f3647529260f5696
LucasRinneToh <lucasrinnetoh@gmail.com>
2021-07-30 01:47:21 +0200

    [FEAT] Added props to TransparentConstruction

--- BE18/TransparentConstruction.cs
+++ BE18/TransparentConstruction.cs
@@ -12,36 +12,36 @@ namespace Bison.Core.BE18
     {
         public TransparentConstruction(
             double area = 0, 
-            double u = 1.2, 
+            double u_value = 1.2,
+            double b_value = 1,
+            double units = 1,
+            double orientation = 0,
+            double slope = 90,
+            double glazingPercentage = 0.73,
+            double g_value = 0.53,
+            double shadingFactor = 1,
             double td_in = 20,
             double td_out = -12,
             string sfb = null,
-            double has_b = 1,
-            double no = 1,
-            double orient = 0,
-            double slope = 90,
-            double ff = 1,
-            double g = 0.53,
-            double fc = 1,
             bool ot = false,
             Shading Shading = null
             )
         {
             this.Area = area;
-            this.U_Value = u;
-            this.part = 0;
+            this.U_Value = u_value;
             this.DimTempIndoor = td_in;
             this.DimTempOutdoor = td_out;
             this.sfb = sfb;
-            this.b_Value = has_b;
-            this.Units = no;
-            this.Orientation = orient;
+            this.b_Value = b_value;
+            this.Units = units;
+            this.Orientation = orientation;
             this.Slope = slope;
-            this.ff = ff;
-            this.g = g;
-            this.fc = fc;
-            this.ot = ot;
+            this.GlazingPercentage = glazingPercentage;
+            this.g_Value = g_value;
+            this.ShadingFactor = shadingFactor;
+            this.UsedInOverheating = ot;
             this.Shading = Shading;
+            this.part = 0;
         }
 
         // Area [m2]
@@ -68,7 +68,7 @@ namespace Bison.Core.BE18
         [FieldAttribute(XmlElementName = "sfb", ValueType = Attributes.ValueType.String)]
         public string sfb { get; set; }
 
-        // b-value (0-1)
+        // b-value (temperature-factor) (0-1)
         [FieldAttribute(XmlElementName = "has_b", ValueType = Attributes.ValueType.Double)]
         public double b_Value { get; set; }
 
@@ -82,14 +82,25 @@ namespace Bison.Core.BE18
 
         // Vertical slope [deg]
         [FieldAttribute(XmlElementName = "slope", ValueType = Attributes.ValueType.Double)]
-        public double Slope { get; set; } 
+        public double Slope { get; set; }
 
+        // Glazing percentage (Ff) (0-1) [-]
+        [FieldAttribute(XmlElementName = "ff", ValueType = Attributes.ValueType.Double)]
+        public double GlazingPercentage { get; set; }
 
-        public double ff { get; set; } 
-        public double g { get; set; } // g-value
-        public double fc { get; set; }
-        public bool ot { get; set; } // Flag for use in overtemperature calculation
+        // Heat transfer coefficient (g-value) (0-1) [-]
+        [FieldAttribute(XmlElementName = "g", ValueType = Attributes.ValueType.Double)]
+        public double g_Value { get; set; }
 
+        // Shading factor (Fc) (0-1) [-] // No shading = 1
+        [FieldAttribute(XmlElementName = "fc", ValueType = Attributes.ValueType.Double)]
+        public double ShadingFactor { get; set; }
+
+        // Use transparent construction in overheating calculation?
+        [FieldAttribute(XmlElementName = "ot", ValueType = Attributes.ValueType.Double)]
+        public bool UsedInOverheating { get; set; }
+
+        // Shading
         [FieldAttribute(XmlElementName = "has_shading", ValueType = Attributes.ValueType.OneToOne)]
         public Shading Shading { get; set; }
     }
