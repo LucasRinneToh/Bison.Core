000a8dca5f42439e6a790cc9a0da2fedbaf90134
LucasRinneToh <lucasrinnetoh@gmail.com>
2021-08-08 14:48:19 +0200

    [FIX] Added 'Internal Heat' namespace

--- BE18/Building.cs
+++ BE18/Building.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Bison.Core.BE18.Attributes;
 using Bison.Core.BE18.Elements.ClimateScreen;
+using Bison.Core.BE18.Elements.InternalHeat;
 using Bison.Core.BE18.Elements.DomesticHotWater;
 using Bison.Core.BE18.Elements.Ventilation;
 using Bison.Core.BE18.Elements.HeatingSystems;
--- BE18/Elements/InternalHeat/UsageLoad.cs
+++ BE18/Elements/InternalHeat/UsageLoad.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Bison.Core.BE18.Attributes;
 
-namespace Bison.Core.BE18
+namespace Bison.Core.BE18.Elements.InternalHeat
 {
     [ModelAttribute(ElementName = "USAGE_LOAD")]
     public class UsageLoad : BeElement
